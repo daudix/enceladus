68eebb219438fe4473aef94101dc1825a84d760e
kevadesu <kevadesu@asuslap.top>
2025-05-12 00:44:35 +0200

    Revert "Remove branding, use chrome instead of vivaldi"
    This reverts commit 05b60eeb167d0a9626055b6b455cd3c75ca5c204.

---
+++ files/scripts/branding.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+# Taken from TyrianOS: https://github.com/Icycoide/TyrianOS/blob/54d1df9ad43f7a27ba7c7cb4639b8ac6459052f0/build_files/branding.sh
+
+source /usr/lib/os-release || false
+
+IMAGE_PRETTY_NAME="Enceladus"
+IMAGE_LIKE="fedora"
+HOME_URL="https://daudix.one/enceladus/"
+SUPPORT_URL="https://github.com/daudix/enceladus/issues/"
+BUG_SUPPORT_URL="https://github.com/daudix/enceladus/issues/"
+CODE_NAME="Lyra"
+# VERSION="${VERSION:-00.00000000}"
+IMAGE_NAME=enceladus
+IMAGE_VENDOR=daudix
+
+
+# OS Release File
+sed -i "s|^NAME=.*|NAME=\"Enceladus\"|" /usr/lib/os-release
+sed -i "s|^PRETTY_NAME=.*|PRETTY_NAME=\"${IMAGE_PRETTY_NAME} ${VERSION} (${CODE_NAME} / FROM Fedora 41 Silverblue)\"|" /usr/lib/os-release
+sed -i "s|^HOME_URL=.*|HOME_URL=\"$HOME_URL\"|" /usr/lib/os-release
+sed -i "s|^SUPPORT_URL=.*|SUPPORT_URL=\"$SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^BUG_REPORT_URL=.*|BUG_REPORT_URL=\"$BUG_SUPPORT_URL\"|" /usr/lib/os-release
+sed -i "s|^DEFAULT_HOSTNAME=.*|DEFAULT_HOSTNAME=\"enceladus\"|" /usr/lib/os-release
+sed -i "s|^VERSION_CODENAME=.*|VERSION_CODENAME=\"Lyra\"|" /usr/lib/os-release
